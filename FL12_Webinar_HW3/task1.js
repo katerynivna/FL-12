0dcc83970411608ae4771c3e0f3a283b16b0ebc7
Kateryna Ovcharenko <47566936+katerynivna@users.noreply.github.com>
2020-02-24 17:37:23 +0200

    Add files via upload

---
+++ FL12_Webinar_HW3/task1.js
@@ -0,0 +1,20 @@
+function Card(suit, rank) {
+    this.suit = suit;
+    this.rank = rank;
+    //this.isFaceCard = isFaceCard;
+}
+function deck() {
+    this.suit = ['hearts', 'diamonds', 'clubs', 'spades'];
+    this.rank = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
+    let cards = [];
+
+    for(let r = 0; r < this.rank.length; r++) {
+        for(let s = 0; s < this.suit.length; s++) {
+            cards.push(new Card(this.suit[s], this.rank[r]));
+        }
+    }
+    return cards;
+
+};
+
+console.log(deck());
