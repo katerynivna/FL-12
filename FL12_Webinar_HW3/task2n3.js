f1cec138019694efffcf596f328020b2ee47d108
Kateryna Ovcharenko <47566936+katerynivna@users.noreply.github.com>
2020-02-25 16:44:11 +0200

    Update task2n3.js

---
+++ FL12_Webinar_HW3/task2n3.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const ONE_YEAR = 31556952000;
+
+class Employee {
+    constructor (
+        id,
+        fisrtName,
+        lastName,
+        birthday,
+        salary,
+        position,
+        department
+    ) {
+        this.id = id;
+        this.fisrtName = fisrtName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+        Employee.EMPLOYEES.push(this);
+    }
+
+    get age() {
+        let nowDate = new Date();
+        let ageInYears = parseInt((Date.parse(nowDate) - Date.parse(this.birthday)) / ONE_YEAR);
+        return ageInYears;
+    }
+
+    get fullName() {
+        return `${this.fisrtName} ${this.lastName}`;
+    }
+
+    static EMPLOYEES = [];
+
+    changeDepartment(newDepartment) {
+        this.department = newDepartment;
+    }
+    changePosition(newPosition) {
+        this.position = newPosition;
+    }
+    changeSalary(newSalary) {
+        this.salary = newSalary;
+    }
+
+    quit() {
+        let arrayIndex = Employee.EMPLOYEES.indexOf(this);
+        Employee.EMPLOYEES.splice(arrayIndex, 1);
+    }
+
+}
+
+let Kate = new Employee(1, 'Kate', 'Ovch', '1993/10/07', '100', 'none', 'testDept');
+let Olga = new Employee(2, 'olg', 'olgov', '1982/05/10', 200, 'position2', 'test');
+
+console.log(Olga.quit());
+
