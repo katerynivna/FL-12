854650734c5a5d1e71f69f56a6a87cb40355e8e5
Kateryna Ovcharenko <47566936+katerynivna@users.noreply.github.com>
2020-02-27 17:32:25 +0200

    Update hw4.js

--- FL12_Webinar_HW4/js/hw4.js
+++ FL12_Webinar_HW4/js/hw4.js
@@ -1,20 +1,31 @@
 const testArr = [1, 2, 3, 4, 56, 7, 8, 76, 5, 241, 5, 356, 567, 2];
+const testNumber = '0123456789';
 
+//task 1
 function maxElement(array) {
     let max = Math.max(...array);
     return max;
 }
+//console.log(maxElement(testArr));
 
-console.log(maxElement(testArr));
-
+//task 2
 function copyArray(array) {
     let copiedArr = [];
     array.forEach((element) => { copiedArr.push(element) });
-    /*let copiedArr = [];
-    for (let value of array) {
-        copiedArr.push(value);
-    }*/
     return copiedArr;
 }
+//console.log(copyArray(testArr));
 
-console.log(copyArray(testArr));
+//task 3
+//task 4
+
+
+//task 5
+function findUniqueElements(array) {
+    let filtrArr = [...new Set(array)];
+    return filtrArr;
+}
+//console.log(findUniqueElements(testArr));
+
+//task 6
+function hideNumber
